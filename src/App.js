31e4fd8580ba6adb346fc69306a32ea3368e9d20
Malina Freeman <mgfreema@usc.edu>
2024-12-03 22:10:15 -0800

    Making nav bar dynamic, integrating more pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CreateProfile from "./pages/CreateProfile";
 import HomePage from "./pages/HomePage";
-
 import Explore from "./pages/Explore";
-
 import Login from "./pages/Login";
-
 import './App.css';
 import SettingsPage from './pages/Settings';
 import ChangePasswordPage from './pages/PasswordChange';
 
 function App() {
+  // use react states to check if user is logged in
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userId = localStorage.getItem('uid');
+    setLoggedIn(!!userId);
+  }, []);
+
   return (
     <Router>
       <div>
-        {/* Add a navigation bar for convenience */}
+        {/* Navbar links change depending on if user is signed in or not */}
         <nav className="navbar">
-          <a href="/">Home</a> | <a href="/createProfile">Create Profile</a> | <a href="/explore">Explore</a> | <a href="/explore">Login</a>
-
-          
+          <Link to="/">Home</Link> | 
+          <Link to="/explore">Explore</Link> | 
+          {loggedIn ? (
+            <>
+              <Link to="/createProfile">Create Profile</Link> | 
+              <Link to="/settings">Settings</Link> | 
+              
+            </>
+          ) : (
+            <>
+              <Link to="/Login">Login</Link> | 
+              <Link to="/register">Registration</Link>
+            </>
+          )}
         </nav>
-        
         {/* Define your routes */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/createProfile" element={< CreateProfile />} />
-
           <Route path="/explore" element={< Explore />} />
-
           <Route path="/settings" element={< SettingsPage />} />
           <Route path="/settings/passwordChange" element={< ChangePasswordPage />} />
           <Route path="/Login" element={< Login />}></Route>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,10 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to Our App!</h1>
-      <p>This is the homepage. Click "Create Profile" to start!</p>
+      <h1>Welcome to TrojanMatch!</h1>
+      <p>At a big school like USC, it can be hard to meet new people<br></br><br></br> and make lasting friendships.
+        Using TrojanMatch, you can<br></br><br></br> create a personalized profile to connect with other similar<br></br><br></br> students
+        at USC and build a lifelong Trojan Network!</p>
     </div>
   );
 };
