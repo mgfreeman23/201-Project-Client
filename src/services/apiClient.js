62b17ec4804f8fc560e65f0452f760245856ed9e
Nidhi Munikote <nmunikote@gmail.com>
2024-12-08 14:09:45 -0800

    registration, createprofile, explore, and logout work

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:8080'; // Adjust based on your backend
 
 export const saveProfile = async (profileData) => {
-  const response = await axios.post(`${BASE_URL}/profile`, profileData);
+  console.log(`/profile`, profileData);
+  const response = await axios.post(`/CSCI201-Final-Project-Server/profile`, profileData);
   return response.data;
 };
+
